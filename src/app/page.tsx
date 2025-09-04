5bae6a4ed2c093b94a70216eaadec6cf882e7786
Joshua Speelman <joshua.m.speelman@gmail.com>
2025-09-04 21:26:15 +1000

    setup auth/page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold">vrtx</h1>
+      <h2>
+        hi this page needs work. needs to be a form. i will add a skeleton for
+        this soon
+      </h2>
     </main>
   );
 }
